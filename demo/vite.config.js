476ccca113906766864f53b131c682f5c118a9bc
RominaBetzabeVallejos <romina.vallejos@fiuni.edu.py>
2026-09-13 23:34:47 -0300

    Fix Tailwind v4 build issue

--- demo/vite.config.js
+++ demo/vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite'
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: '/Tarea_Mini_ERP/',
-  server: { port: 5173, open: true
-  },
-  build: {outDir: 'dist',sourcemap: true}
+  plugins: [
+    tailwindcss(),
+  ],
 })
