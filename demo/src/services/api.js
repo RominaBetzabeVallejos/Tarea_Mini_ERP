85cd431b4774a95f9cea965dfabc481b1ce2b883
RominaBetzabeVallejos <romina.vallejos@fiuni.edu.py>
2026-09-13 22:39:20 -0300

    Entrega de Tarea

--- demo/src/services/api.js
+++ demo/src/services/api.js
@@ -2,14 +2,12 @@ const API_URL = 'https://minierp.rbnetto.dev/api';
 
 export const authAPI = {
   async login(email, password) {
-    
     let res = await fetch(`${API_URL}/token/`, {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({ username: email, email, password })
     });
 
-    
     if (res.status === 404) {
       res = await fetch(`${API_URL}/login/`, {
         method: 'POST',
@@ -18,20 +16,32 @@ export const authAPI = {
       });
     }
 
-    if (!res.ok) throw new Error('Credenciales incorrectas o error de servidor');
+    if (!res.ok) throw new Error('Credenciales incorrectas');
     return res.json();
   }
 };
 
 export const productsAPI = {
   async getProducts(token) {
-    const res = await fetch(`${API_URL}/products/`, {
+    // Intenta primero con /products/products/ y luego con /products/
+    let res = await fetch(`${API_URL}/products/products/`, {
       method: 'GET',
       headers: {
         'Content-Type': 'application/json',
         'Authorization': `Bearer ${token}`
       }
     });
+
+    if (res.status === 404) {
+      res = await fetch(`${API_URL}/products/`, {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${token}`
+        }
+      });
+    }
+
     if (!res.ok) throw new Error('Error al obtener productos');
     return res.json();
   }
